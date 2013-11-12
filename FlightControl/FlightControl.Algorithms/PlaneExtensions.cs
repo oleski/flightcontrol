5ebf6dee202e4393098350ba1b95daec3bbb74c8
jladvual <jladuval@gmail.com>
2013-11-12 23:30:28 +1300

    added an algorithsm project

---
+++ FlightControl/FlightControl.Algorithms/PlaneExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace FlightControl.Algorithms
+{
+    using Model;
+
+    public static class PlaneExtensions
+    {
+        public static bool CanLand(this Plane plane, Point landingStrip)
+        {
+            var currentPos = plane.Position;
+            return true;
+        }
+    }
+}
